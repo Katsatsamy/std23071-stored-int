fb863e9151d10f9cf09f573ffb0f1b3b5eb5f2e8
Katsatsamy <velonyainaemilie@gmail.com>
2025-07-07 10:13:27 +0300

    feat: finishing doing storedIntController class

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/StoredIntCOntroller.java
@@ -0,0 +1,38 @@
+package com.example.demo.endpoint.rest.controller.health;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+@RestController
+public class StoredIntCOntroller {
+    private final Path FILE_PATH = Paths.get("src","data","stored-int.txt");
+
+    @GetMapping("/stored-int")
+    public ResponseEntity<Integer> getStoredInt() {
+        try{
+            Files.createDirectories(FILE_PATH.getParent());
+
+            if(Files.exists(FILE_PATH)){
+                String content = Files.readString(FILE_PATH);
+                int storedInt = Integer.parseInt(content);
+                return ResponseEntity.ok(storedInt);
+            }else{
+                int randomNumber = new Random().nextInt(1000000);
+
+                Files.writeString(FILE_PATH, String.valueOf(randomNumber));
+                return ResponseEntity.ok(randomNumber);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
